57632da1bf603f5f3e88538fb14117a9b5e6445d
Yeray Gonzalez bartolome <ygonzale@c4r6s2.42urduliz.com>
2022-11-24 10:37:18 +0100

    1

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 10:37:28 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/11/23 14:51:55 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/11/24 10:36:04 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,11 @@ void	ft_push_swap(int argc, t_list *list_a, t_list *list_b)
 	}
 	else
 	{
-		/* pa(&list_a, &list_b);
-		pb(&list_a, &list_b); */
+		printf("Primer numer: %d\n", list_a->number);
+		printf("Segundo numer: %d\n", list_a->next->number);
+		printf("Tercero numer: %d\n", list_a->next->next->number);
+		printf("Cuarto numer: %d\n", list_a->next->next->next->number);
+		printf("Quinto numer: %d\n", list_a->next->next->next->next->number);
 		printf("muchos argumentos\n");
 	}
 }
@@ -51,5 +54,10 @@ int	main(int argc, char **argv)
 	check_arg_errors(argc);
 	do_list(argv, &list_a, &stack);
 	ft_push_swap(argc, list_a, list_b);
+/* 	printf("primero de la lista A: %d\n", list_a).number;
+	printf("segundo de la lista A: %d\n", (list_a).next)->number;
+	printf("tercero de la lista A: %d\n", ((list_a)->next)->next)->number;
+	printf("cuarto de la lista A: %d\n", (((list_a)->next)->next)->next)->number;
+	printf("ultimo de la lista A: %d\n", ((((list_a)->next)->next)->next)->next)->number; */
 	return (0);
 }
--- src/order_few.c
+++ src/order_few.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:56:06 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/11/23 15:27:20 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/11/24 10:30:49 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,6 @@ void	order_5(t_list **list_a, t_list **list_b)
 		list_a = list_a->next;
 	} */
 	ar5_first(list_a, list_b);
-	printf("primero de la lista A: %d\n", (*list_a)->number);
-	printf("segundo de la lista A: %d\n", ((*list_a)->next)->number);
-	printf("tercero de la lista A: %d\n", (((*list_a)->next)->next)->number);
-	printf("cuarto de la lista A: %d\n", ((((*list_a)->next)->next)->next)->number);
 	ar5_second(list_a, list_b);
 	printf("primero de la lista A: %d\n", (*list_a)->number);
 	printf("segundo de la lista A: %d\n", ((*list_a)->next)->number);
@@ -56,24 +52,13 @@ void	order_5(t_list **list_a, t_list **list_b)
 
 void	ar5_first(t_list **list_a, t_list **list_b)
 {
-	//printf("1\n");
-/* 	printf("primer numero de B: %d\n", (*list_b)->number);
-	printf("segundo numerode B: %d\n", ((*list_b)->next)->number); */
-	//printf("primernumer: %d\n", ((list_b->next)->next)->number);
-/* 	printf("primero de la lista A: %d\n", (*list_a)->number);
-	printf("segundo de la lista A: %d\n", ((*list_a)->next)->number);
-	printf("ultimo de la lista A: %d\n", (((*list_a)->next)->next)->number); */
-	printf("entra en first\n");
 	if ((*list_b)->number > (((*list_a)->next)->next)->number)
 	{
-		printf("2\n");
 		pa(list_a, list_b);
 		ra(list_a);
-		printf("hacepa\n");
 	}
 	else if ((*list_b)->number > ((*list_a)->next)->number)
 	{
-		printf("3\n");
 		rra(list_a);
 		pa(list_a, list_b);
 		ra(list_a);
@@ -81,55 +66,39 @@ void	ar5_first(t_list **list_a, t_list **list_b)
 	}
 	else if ((*list_b)->number > (*list_a)->number)
 	{
-		printf("entra\n");
 		pa(list_a, list_b);
 		sa(list_a);
 	}
 	else
 		pa(list_a, list_b);
-	printf("sale de first\n");
-	/* if (list_a->number < (list_a->next)->number)
-		return ; */
 }
 
 void	ar5_second(t_list **list_a, t_list **list_b)
 {
-	printf("entra en second\n");
-	printf("primer numero de B (2): %d\n", (*list_b)->number);
 	if ((*list_b)->number > (((((*list_a)->next)->next)->next)->number))
 	{
 		pa(list_a, list_b);
 		ra(list_a);
 	}
-	if ((*list_b)->number > ((((*list_a)->next)->next)->number))
+	else if ((*list_b)->number > ((((*list_a)->next)->next)->number))
 	{
 		rra(list_a);
 		pa(list_a, list_b);
 		ra(list_a);
 		ra(list_a);
 	}
-	if ((*list_b)->number > (((*list_a)->next)->number))
+	else if ((*list_b)->number > (((*list_a)->next)->number))
 	{
-		printf("entra\n");
 		ra(list_a);
-		while ((*list_a))
-		{
-			printf("NUMBER: %d\n", (*list_a)->number);
-			(*list_a) = (*list_a)->next;
-		}
 		pa(list_a, list_b);
 		sa(list_a);
 		rra(list_a);
 	}
-	if ((*list_b)->number > ((*list_a)->number))
+	else if ((*list_b)->number > ((*list_a)->number))
 	{
-		printf("segundo numero\n");
 		pa(list_a, list_b);
 		sa(list_a);
 	}
-/* 	else
-		pa(list_a, list_b); */
-	printf("final second\n");
-	/* if (list_a->number < (list_a->next)->number)
-		return ; */
+	else
+		pa(list_a, list_b);
 }
